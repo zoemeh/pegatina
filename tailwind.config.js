f852579fd5302caf48c738341228246fb75bc511
Eduardo Cruz-Betances <eduardomcbt@gmail.com>
2021-12-18 21:40:25 -0400

    init :)

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/views/**/*.html.slim',
+    './app/components/**/*.html.slim',
+    './app/components/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js'
+  ]
+}
